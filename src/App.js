5acb2d9bbcf3670207efb438592b7f5d7418242f
saqlain Abbas <saqlainabbas744@gmail.com>
2024-09-10 09:14:41 -0700

    TD-001 Update user and sidebar components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css'
-import Header from './components/Header.js'
+import Sidebar from './components/Sidebar.js'
 import Main from './components/Main.js'
 import User from './components/User.js'
 import AddNewTodo from './components/AddNewTodo.js'
 import Calender from './components/Calender.js'
-import Projects from './components/Projects.js'
 import EditTodo from './components/EditTodo.js'
 import Todo from './components/Todo.js'
+import Next7Days from './components/Next7Days.js'
+import RenameProject from './components/RenameProject.js'
+import AddNewProject from './components/AddNewProject.js'
 
 function App(){
   return(
     <div className='App'>
-      <Header>
+      <Sidebar>
         <User/>
         <AddNewTodo/>
         <Calender/>
-        <Projects/>
-      </Header>
+        <AddNewProject/>
+        <RenameProject/>
+      </Sidebar>
       <Main>
         <Todo/>
+        <Next7Days/>
         <EditTodo/>
       </Main>
     </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,17 @@
 import React from "react";
+import logo from '../images/logo.png'
 
 function User(){
 
   return(
     <div className="User">
-      User
+        <div className="logo">
+            <img src={logo}/>
+        </div>
+        <div className="info">
+            <p>Saqlain Abbas</p>
+            <a href="#">Logout</a>
+        </div>
     </div>
   )
 }
