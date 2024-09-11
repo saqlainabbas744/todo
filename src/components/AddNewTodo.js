10434be4617d9b6e63e4a080d93495f35148be2c
saqlain Abbas <saqlainabbas744@gmail.com>
2024-09-11 09:29:16 -0700

    TD-002 Add modal for add new task component

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 function AddNewTodo(){
+  const [showModal, setShowModal] = useState(false)
 
   return(
     <div className="AddNewTodo">
-      AddNewTodo
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>
+          + New Todo
+        </button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+          Hello World
+          <button onClick={() => setShowModal(false)}>
+            Hide
+          </button>
+        </div>
+      </Modal>
     </div>
   )
 }
